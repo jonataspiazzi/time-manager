d204ed06273ffcfa23928684bdd0b5d0a21b3403
Jonatas Piazzi <piazzi_7@hotmail.com>
2018-03-16 16:09:20 -0300

    First commit.

---
+++ src/TimeManager/View.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TimeManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class View : Window
+    {
+        public View()
+        {
+            InitializeComponent();
+            ((ViewModel)DataContext).Window = this;
+        }
+
+        private void OnClosing(object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;
+            Visibility = Visibility.Hidden;
+        }
+    }
+}
